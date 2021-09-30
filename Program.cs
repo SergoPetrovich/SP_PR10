3e75ea3bce429c0dcbd6e513398845ceff88e051
SPS <tk34moodle@tk34.mskobr.ru>
2021-09-30 14:52:35 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SP_PR10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
